e29eca6c17a1d91d1a8e8f847435f0bac2f3a956
Digvijay-Chaudhari <digvijay007chaudhari@gmail.com>
2022-03-16 19:46:58 +0530

    Typescript code

---
+++ typescript Bank Task/App/IAccount.ts
@@ -0,0 +1,13 @@
+export interface IAccount
+{
+    name:string;
+    emailID:string;
+    age:number;
+    location:string;
+    state:string;
+    country:string;
+    accountType:string;
+    accountNumber:string;
+    balance:number;
+    minimumBalance:number;
+}
